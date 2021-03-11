c1b60602d56fcbb24eb65ccb4bcb451cc8ae42a7
jlbuild <juliabuildbot@gmail.com>
2021-03-11 12:20:59 +0000

    polymake_jll build 400.300.1+0

--- src/wrappers/x86_64-linux-gnu-cxx11.jl
+++ src/wrappers/x86_64-linux-gnu-cxx11.jl
@@ -28,13 +28,13 @@ function __init__()
     JLLWrappers.@init_library_product(
         libpolymake,
         "lib/libpolymake.so",
-        nothing,
+        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libpolymake_apps_rt,
         "lib/libpolymake-apps-rt.so",
-        nothing,
+        RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_executable_product(
