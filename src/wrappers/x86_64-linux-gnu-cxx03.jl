49f51fc36e4fbff0d615edd01ad19e54d55b280c
jlbuild <juliabuildbot@gmail.com>
2021-09-08 17:03:29 +0000

    polymake_jll build 400.400.2+0

--- src/wrappers/x86_64-linux-gnu-cxx03.jl
+++ src/wrappers/x86_64-linux-gnu-cxx03.jl
@@ -10,6 +10,7 @@ using Perl_jll
 using bliss_jll
 using boost_jll
 using cddlib_jll
+using lib4ti2_jll
 using lrslib_jll
 using normaliz_jll
 JLLWrappers.@generate_wrapper_header("polymake")
@@ -19,7 +20,7 @@ JLLWrappers.@declare_library_product(libpolymake_apps_rt, "libpolymake-apps-rt.s
 JLLWrappers.@declare_executable_product(polymake)
 JLLWrappers.@declare_executable_product(polymake_config)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, GMP_jll, MPFR_jll, FLINT_jll, PPL_jll, Perl_jll, bliss_jll, boost_jll, cddlib_jll, lrslib_jll, normaliz_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, GMP_jll, MPFR_jll, FLINT_jll, PPL_jll, Perl_jll, bliss_jll, boost_jll, cddlib_jll, lib4ti2_jll, lrslib_jll, normaliz_jll)
     JLLWrappers.@init_file_product(
         generate_deps_tree,
         "share/polymake/generate_deps_tree.jl",
