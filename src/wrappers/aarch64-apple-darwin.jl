e9833b7d8e8d4198338fc695903c6a76f39f2b3a
jlbuild <juliabuildbot@gmail.com>
2023-11-08 11:31:41 +0000

    polymake_jll build 400.1100.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -5,6 +5,7 @@ using LLVMOpenMP_jll
 using GMP_jll
 using MPFR_jll
 using FLINT_jll
+using MongoC_jll
 using PPL_jll
 using Perl_jll
 using SCIP_jll
@@ -14,21 +15,21 @@ using cddlib_jll
 using lrslib_jll
 using normaliz_jll
 JLLWrappers.@generate_wrapper_header("polymake")
-JLLWrappers.@declare_library_product(libpolymake, "@rpath/libpolymake.4.10.dylib")
-JLLWrappers.@declare_library_product(libpolymake_apps_rt, "@rpath/libpolymake-apps-rt.4.10.dylib")
+JLLWrappers.@declare_library_product(libpolymake, "@rpath/libpolymake.4.11.dylib")
+JLLWrappers.@declare_library_product(libpolymake_apps_rt, "@rpath/libpolymake-apps-rt.4.11.dylib")
 JLLWrappers.@declare_executable_product(polymake)
 JLLWrappers.@declare_executable_product(polymake_config)
 function __init__()
-    JLLWrappers.@generate_init_header(LLVMOpenMP_jll, GMP_jll, MPFR_jll, FLINT_jll, PPL_jll, Perl_jll, SCIP_jll, bliss_jll, boost_jll, cddlib_jll, lrslib_jll, normaliz_jll)
+    JLLWrappers.@generate_init_header(LLVMOpenMP_jll, GMP_jll, MPFR_jll, FLINT_jll, MongoC_jll, PPL_jll, Perl_jll, SCIP_jll, bliss_jll, boost_jll, cddlib_jll, lrslib_jll, normaliz_jll)
     JLLWrappers.@init_library_product(
         libpolymake,
-        "lib/libpolymake.4.10.dylib",
+        "lib/libpolymake.4.11.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libpolymake_apps_rt,
-        "lib/libpolymake-apps-rt.4.10.dylib",
+        "lib/libpolymake-apps-rt.4.11.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/x86_64-unknown-freebsd.jl
+++ src/wrappers/x86_64-unknown-freebsd.jl
@@ -5,6 +5,7 @@ using LLVMOpenMP_jll
 using GMP_jll
 using MPFR_jll
 using FLINT_jll
+using MongoC_jll
 using PPL_jll
 using Perl_jll
 using SCIP_jll
@@ -14,12 +15,12 @@ using cddlib_jll
 using lrslib_jll
 using normaliz_jll
 JLLWrappers.@generate_wrapper_header("polymake")
-JLLWrappers.@declare_library_product(libpolymake, "libpolymake.so.4.10")
-JLLWrappers.@declare_library_product(libpolymake_apps_rt, "libpolymake-apps-rt.so.4.10")
+JLLWrappers.@declare_library_product(libpolymake, "libpolymake.so.4.11")
+JLLWrappers.@declare_library_product(libpolymake_apps_rt, "libpolymake-apps-rt.so.4.11")
 JLLWrappers.@declare_executable_product(polymake)
 JLLWrappers.@declare_executable_product(polymake_config)
 function __init__()
-    JLLWrappers.@generate_init_header(LLVMOpenMP_jll, GMP_jll, MPFR_jll, FLINT_jll, PPL_jll, Perl_jll, SCIP_jll, bliss_jll, boost_jll, cddlib_jll, lrslib_jll, normaliz_jll)
+    JLLWrappers.@generate_init_header(LLVMOpenMP_jll, GMP_jll, MPFR_jll, FLINT_jll, MongoC_jll, PPL_jll, Perl_jll, SCIP_jll, bliss_jll, boost_jll, cddlib_jll, lrslib_jll, normaliz_jll)
     JLLWrappers.@init_library_product(
         libpolymake,
         "lib/libpolymake.so",
